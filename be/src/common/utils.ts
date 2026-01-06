1c964cc484e93f5aae83bb67aba4fa72fa92ef46
benbenSapir <benbenfsapir@gmail.com>
2026-01-06 11:59:26 +0200

    .

--- be/src/common/utils.ts
+++
@@ -1,4 +0,0 @@
-export function removeNullFields(obj: any) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return Object.fromEntries(Object.entries(obj).filter(([_key, value]) => value !== null)) as any;
-}
