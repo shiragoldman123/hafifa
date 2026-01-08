f60b78262c839a94059d8efb9574f9b92341aff1
unknown <shira7206@gmail.com>
2026-01-08 17:18:29 +0200

    add docker configuration file

--- be/src/main.ts
+++ be/src/main.ts
@@ -9,10 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(logger('dev'));
-  app.enableCors({
-    origin: "http://localhost:5173"
-  })
-
+  
+    app.enableCors({
+    origin: process.env.NODE_ENV === 'production' 
+      ? [process.env.FRONTEND_URL || 'http://localhost:5173']
+      : 'http://localhost:5173',
+    credentials: true
+  });
   const config = new DocumentBuilder()
     .setTitle(`${envConfig.metaData.systemName + envConfig.metaData.serviceName} API`)
     .setDescription(envConfig.metaData.description)
